aafe240b5ae8fa002a264b01fca53f8f37b8aff6
Ed Brannin <edbrannin@gmail.com>
2017-07-14 09:36:37 -0400

    Automatically virtualenv-install pushbullet-cli if needed

--- check.sh
+++ check.sh
@@ -1,7 +1,14 @@
+function notify {
+  [ -d _env ] || virtualenv _env
+  . _env/bin/activate
+  pip install -r requirements.txt
+  pb push -t "Chrono Shop Update" -u https://chrono.gg/shop < diff.txt
+}
+
 [ -f shop.json ] && mv shop.json shop-old.json
 [ -f result.json ] && mv result.json result-old.json
 curl -s https://api.chrono.gg/shop > shop.json
 jq '.[] | select(.sold_out? != true) | { status: .status, name: .name?, price: .price, url: .url, platforms: .platforms }' shop.json > result.json
 echo "< Old | New >" > diff.txt
-diff result{-old,}.json >> diff.txt || pb push -t "Chrono Shop Update" -u https://chrono.gg/shop < diff.txt
+diff result{-old,}.json >> diff.txt || notify
 
