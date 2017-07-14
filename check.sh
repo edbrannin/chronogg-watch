c69c2fb15282880cd6510a35b6587ede21f95360
Ed Brannin <edbrannin@gmail.com>
2017-07-14 11:01:07 -0400

    subshells don't work so hot

--- check.sh
+++ check.sh
@@ -4,11 +4,12 @@ function notify {
   [ -d _env ] || virtualenv _env
   . _env/bin/activate
   pip install -r requirements.txt
-  pb push -t "Chrono Shop Update" -u https://chrono.gg/shop <(
+  (
     echo "< Old | New >"
     cat $CACHE_DIR/diff.txt
     echo "I am: $(whoami)"
-    )
+  ) > $CACHE_DIR/message.txt
+  pb push -t "Chrono Shop Update" -u https://chrono.gg/shop <$CACHE_DIR/message.txt
 }
 
 [ -d $CACHE_DIR ] || mkdir $CACHE_DIR
