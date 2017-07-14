0a538a67216a812f67cc1bf8f714fa25f03a0c59
Ed Brannin <edbrannin@gmail.com>
2017-07-14 09:39:58 -0400

    Keep intermediate files in cache/

--- check.sh
+++ check.sh
@@ -1,14 +1,18 @@
+CACHE_DIR=cache
+
 function notify {
   [ -d _env ] || virtualenv _env
   . _env/bin/activate
   pip install -r requirements.txt
-  pb push -t "Chrono Shop Update" -u https://chrono.gg/shop < diff.txt
+  pb push -t "Chrono Shop Update" -u https://chrono.gg/shop < $CACHE_DIR/diff.txt
 }
 
-[ -f shop.json ] && mv shop.json shop-old.json
-[ -f result.json ] && mv result.json result-old.json
-curl -s https://api.chrono.gg/shop > shop.json
-jq '.[] | select(.sold_out? != true) | { status: .status, name: .name?, price: .price, url: .url, platforms: .platforms }' shop.json > result.json
-echo "< Old | New >" > diff.txt
-diff result{-old,}.json >> diff.txt || notify
+[ -d $CACHE_DIR ] || mkdir $CACHE_DIR
+
+[ -f $CACHE_DIR/shop.json ] && mv $CACHE_DIR/shop.json $CACHE_DIR/shop-old.json
+[ -f $CACHE_DIR/result.json ] && mv $CACHE_DIR/result.json $CACHE_DIR/result-old.json
+curl -s https://api.chrono.gg/shop > $CACHE_DIR/shop.json
+jq '.[] | select(.sold_out? != true) | { status: .status, name: .name?, price: .price, url: .url, platforms: .platforms }' $CACHE_DIR/shop.json > $CACHE_DIR/result.json
+echo "< Old | New >" > $CACHE_DIR/diff.txt
+diff $CACHE_DIR/result{-old,}.json >> $CACHE_DIR/diff.txt || notify
 
