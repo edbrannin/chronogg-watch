8b4a8ac7cc1f93069cd94bd50429c94d7ca57006
Ed Brannin <edbrannin@gmail.com>
2017-07-14 10:44:11 -0400

    Debugging

--- check.sh
+++ check.sh
@@ -4,7 +4,11 @@ function notify {
   [ -d _env ] || virtualenv _env
   . _env/bin/activate
   pip install -r requirements.txt
-  pb push -t "Chrono Shop Update" -u https://chrono.gg/shop < $CACHE_DIR/diff.txt
+  pb push -t "Chrono Shop Update" -u https://chrono.gg/shop <(
+    echo "< Old | New >"
+    cat $CACHE_DIR/diff.txt
+    echo "I am: $(whoami)"
+    )
 }
 
 [ -d $CACHE_DIR ] || mkdir $CACHE_DIR
@@ -13,6 +17,5 @@ function notify {
 [ -f $CACHE_DIR/result.json ] && mv $CACHE_DIR/result.json $CACHE_DIR/result-old.json
 curl -s https://api.chrono.gg/shop > $CACHE_DIR/shop.json
 jq '.[] | select(.sold_out? != true) | { status: .status, name: .name?, price: .price, url: .url, platforms: .platforms }' $CACHE_DIR/shop.json > $CACHE_DIR/result.json
-echo "< Old | New >" > $CACHE_DIR/diff.txt
 diff $CACHE_DIR/result{-old,}.json >> $CACHE_DIR/diff.txt || notify
 
