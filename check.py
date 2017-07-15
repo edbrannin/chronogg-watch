18a81fe554926c4aae25d1cd3c1623651ca23ad5
Ed Brannin <edbrannin@gmail.com>
2017-07-15 10:14:33 -0400

    Better diffs

---
+++ check.py
@@ -0,0 +1,26 @@
+import sys
+import os
+import json
+
+import json_delta
+# import pushbullet_cli
+# import requests
+
+CACHE_DIR="cache"
+
+def read_cache(name):
+    with open(os.path.join(CACHE_DIR, name)) as in_file:
+        return json.load(in_file)
+
+new, old = read_cache('result.json'), read_cache('result-old.json')
+diff = json_delta.diff(new, old, minimal=True)
+
+udiff = json_delta.udiff(new, old, patch=diff, indent=2)
+
+for line in udiff:
+    print line
+
+if udiff:
+    sys.exit(1)
+else:
+    sys.exit(0)
--- check.sh
+++ check.sh
@@ -9,7 +9,8 @@ function notify {
     cat $CACHE_DIR/diff.txt
     echo "I am: $(whoami)"
   ) > $CACHE_DIR/message.txt
-  pb push -t "Chrono Shop Update" -u https://chrono.gg/shop <$CACHE_DIR/message.txt
+  cat $CACHE_DIR/message.txt
+  cat $CACHE_DIR/message.txt | pb push -t "Chrono Shop Update" -u https://chrono.gg/shop 
 }
 
 [ -d $CACHE_DIR ] || mkdir $CACHE_DIR
@@ -17,6 +18,6 @@ function notify {
 [ -f $CACHE_DIR/shop.json ] && mv $CACHE_DIR/shop.json $CACHE_DIR/shop-old.json
 [ -f $CACHE_DIR/result.json ] && mv $CACHE_DIR/result.json $CACHE_DIR/result-old.json
 curl -s https://api.chrono.gg/shop > $CACHE_DIR/shop.json
-jq '.[] | select(.sold_out? != true) | { status: .status, name: .name?, price: .price, url: .url, platforms: .platforms }' $CACHE_DIR/shop.json > $CACHE_DIR/result.json
-diff $CACHE_DIR/result{-old,}.json >> $CACHE_DIR/diff.txt || notify
+jq '[.[] | select(.sold_out? != true) | { status: .status, name: .name?, price: .price, url: .url, platforms: .platforms }]' $CACHE_DIR/shop.json > $CACHE_DIR/result.json
+python check.py >> $CACHE_DIR/diff.txt || notify
 
