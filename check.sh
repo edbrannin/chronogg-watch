451bd0e9404d0333bfa5a2e1e38a40daeaa66ca0
Ed Brannin <edbrannin@gmail.com>
2017-07-16 09:25:11 -0400

    Fix: add shebang so cron doesn't use sh; activate virtualenv for check.py; don't append forever to diff.txt

--- check.sh
+++ check.sh
@@ -1,17 +1,20 @@
-CACHE_DIR=cache
+#!/bin/bash -x
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export CACHE_DIR=$DIR/cache
 
 function notify {
-  [ -d _env ] || virtualenv _env
-  . _env/bin/activate
-  pip install -r requirements.txt
   (
     echo "< Old | New >"
     cat $CACHE_DIR/diff.txt
-    echo "I am: $(whoami)"
   ) > $CACHE_DIR/message.txt
   cat $CACHE_DIR/message.txt
   cat $CACHE_DIR/message.txt | pb push -t "Chrono Shop Update" -u https://chrono.gg/shop 
 }
+
+[ -d _env ] || virtualenv _env
+. _env/bin/activate
+pip install -r requirements.txt
 
 [ -d $CACHE_DIR ] || mkdir $CACHE_DIR
 
@@ -19,5 +22,5 @@ function notify {
 [ -f $CACHE_DIR/result.json ] && mv $CACHE_DIR/result.json $CACHE_DIR/result-old.json
 curl -s https://api.chrono.gg/shop > $CACHE_DIR/shop.json
 jq '[.[] | select(.sold_out? != true) | { status: .status, name: .name?, price: .price, url: .url, platforms: .platforms }]' $CACHE_DIR/shop.json > $CACHE_DIR/result.json
-python check.py >> $CACHE_DIR/diff.txt || notify
+python check.py > $CACHE_DIR/diff.txt || notify
 
